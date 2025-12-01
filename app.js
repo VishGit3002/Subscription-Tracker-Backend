8123f58ddbf029d1dd0308f8eec9e0c2f4f730e3
Vishal Singh <vish.singh3020@gmail.com>
2025-12-01 16:40:36 +0530

    Implement error middleware

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import subscriptionRoutes from './routes/subscription.routes.js';
 import connectToDatabase from "./database/mongoDb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 // Initialize app
 const app = express();
@@ -12,11 +14,14 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/subscriptions', subscriptionRoutes);
+
+app.use(errorMiddleware);
 
 // Default Route
 app.get('/', (req, res) => res.send("Welcome to the Subscription Tracker API"));
