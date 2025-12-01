65ba149ddb6e3f7f0e82eee659716ed22a63e329
Vishal Singh <156598241+VishGit3002@users.noreply.github.com>
2025-12-01 11:33:26 +0530

    configure app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 import express from "express";
 import { PORT } from './config/env.js';
+
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import subscriptionRoutes from './routes/subscription.routes.js';
+import connectToDatabase from "./database/mongoDb.js";
 
+// Initialize app
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/subscriptions', subscriptionRoutes);
 
+// Default Route
 app.get('/', (req, res) => res.send("Welcome to the Subscription Tracker API"));
 
-app.listen(PORT, () => {
+// Server
+app.listen(PORT, async () => {
     console.log(`Server started on port ${PORT}`);
-})
+    await connectToDatabase();
+});
